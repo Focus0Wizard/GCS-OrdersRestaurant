480d3442d4a01ec64b3deaf6a51d95e61ccfc7a0
Focus0Wizard <jhonn.ramirez@ucb.edu.bo>
2025-10-20 07:20:28 -0400

    Added validations

--- RestaurantQA/Entities/Producto.cs
+++ RestaurantQA/Entities/Producto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Restaurant.Entities;
 
@@ -7,12 +8,20 @@ public partial class Producto
 {
     public short Id { get; set; }
 
+    [Required(ErrorMessage = "El nombre del producto es obligatorio.")]
+    [StringLength(20, MinimumLength = 4, ErrorMessage = "El nombre del producto no puede pasar de los 20 caracteres y no ser menos a 4.")]
     public string Nombre { get; set; } = null!;
 
+    [Range(0.01, 1000, ErrorMessage = "El precio debe ser mayor a 0 y menor a 1000.")]
     public decimal Precio { get; set; }
 
+    [Range(0, 150, ErrorMessage = "El stock no puede ser negativo y no mayor a 200.")]
     public int? Stock { get; set; }
+    
+    [Required(ErrorMessage = "La descripcion del producto es obligatoria.")]
+    [StringLength(100, MinimumLength = 5, ErrorMessage = "La descripcion del producto no puede pasar de los 20 caracteres y no ser menos a 4.")]
     public string Descripcion { get; set; }
+    [Range(1, short.MaxValue, ErrorMessage = "La Categoría debe ser un número positivo mayor a cero.")]
     public short CategoriaId { get; set; }
 
     public DateTime? FechaCreacion { get; set; }
--- RestaurantQA/Entities/Repartidore.cs
+++ RestaurantQA/Entities/Repartidore.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Restaurant.Entities;
 
@@ -7,10 +8,21 @@ public partial class Repartidore
 {
     public short Id { get; set; }
 
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
+    [RegularExpression(@"^[A-ZÁÉÍÓÚÑ][a-záéíóúñ]+(?: [A-ZÁÉÍÓÚÑ][a-záéíóúñ]+)*$", 
+        ErrorMessage = "El nombre solo puede contener letras y debe iniciar con mayúscula.")]
+    [StringLength(30, MinimumLength = 3,  ErrorMessage = "El nombre no puede exceder 10 caracteres.")]
     public string Nombre { get; set; } = null!;
-
+    
+    [Required(ErrorMessage = "El apellido es obligatorio.")]
+    [RegularExpression(@"^[A-ZÁÉÍÓÚÑ][a-záéíóúñ]+(?: [A-ZÁÉÍÓÚÑ][a-záéíóúñ]+)*$", 
+        ErrorMessage = "El apellido solo puede contener letras y debe iniciar con mayúscula.")]
+    [StringLength(30, MinimumLength = 3, ErrorMessage = "El apellido no puede exceder 10 caracteres.")]
     public string Apellido { get; set; } = null!;
 
+    [Phone(ErrorMessage = "El número de teléfono no es válido.")]
+    [RegularExpression(@"^[67][0-9]{5,7}$", ErrorMessage = "El número de teléfono debe comenzar con 6 o 7 y tener un total de 7 a 8 dígitos.")]
+    [StringLength(8, MinimumLength = 7, ErrorMessage = "El número de teléfono debe tener entre 7 y 8 dígitos.")]
     public string? Telefono { get; set; }
 
     public string? EstadoEntrega { get; set; }
